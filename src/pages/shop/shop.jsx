1417a3bc243495423248961fbca06980ffe0a5b7
mosidrum <mosiokanga@gmail.com>
2023-03-05 18:50:42 +0100

    products displays

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { PRODUCTS } from '../../products';
+ import { Product } from './product';
+ import './shop.css';
 
 export const Shop = () => {
   return (
     <div className='shop'>
-      <div>
+      <div className='shopTitle'>
         <h1>MyShop</h1>
       </div>
       <div className='products'>
-        
+        {PRODUCTS.map((product) => (<Product data={product}/>
+        ))}
       </div>
     </div>
   )
