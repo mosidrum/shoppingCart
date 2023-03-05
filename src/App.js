40de19d3c86b3fca076496ec8c410134c2f40aba
mosidrum <mosiokanga@gmail.com>
2023-03-05 15:35:41 +0100

    initialized with guthub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar';
+import Shop from './pages/shop/Shop';
+import Cart from './pages/cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element= {<Shop />}/>
+          <Route path='/cart' element= {< Cart />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
