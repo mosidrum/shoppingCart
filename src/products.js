58170e44c378ca17fd09ae2fc222a4c11592e873
mosidrum <mosiokanga@gmail.com>
2023-03-05 18:17:23 +0100

    Product items added

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/1.png'
+import product2 from './assets/2.png'
+import product3 from './assets/3.png'
+import product4 from './assets/4.png'
+import product5 from './assets/5.png'
+import product6 from './assets/6.webp'
+import product7 from './assets/7.webp'
+import product8 from './assets/8.webp'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Iphone',
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: 'Hp Laptop',
+        price: 2000.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: 'Canon Camera',
+        price: 700.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: 'Jean Jacket',
+        price: 100.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: 'LED Light',
+        price: 400.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: 'Designers Shirt',
+        price: 50.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: 'Dolce and Gabana',
+        price: 60.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: 'Face Cap',
+        price: 10.0,
+        productImage: product8,
+    },
+];
