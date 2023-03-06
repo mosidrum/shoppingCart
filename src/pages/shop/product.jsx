cba5a59c5c47d4220e28fc5c46347f9902295fff
mosidrum <mosiokanga@gmail.com>
2023-03-06 09:59:57 +0100

    Adding to cart done

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { ShopContext } from '../../context/shop-context'; 
 
 export const Product = (props) => {
     const {id, productName, price, productImage} = props.data;
+    const { addToCart, cartItems } = useContext(ShopContext);
+
+    const cartItemAmount = cartItems[id]
   return (
     <div className='product'>
-        <img src={productImage} alt='Product image' />
+        <img src={productImage} alt='item pics' />
         <div className='description'>
             <p>
                 <b>{productName}</b>
             </p>
             <p> ${price} </p>
         </div>
+        <button className='addToCartBttn' onClick={() => addToCart(id)}>Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount}) </>}</button>
     </div>
   )
 }
